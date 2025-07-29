affdb806d37a6f33273ecc70537560d5729bdf48
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-07-29 23:57:28 +0530

    Time: 34 ms (28.38%) | Memory: 61 MB (95.95%) - LeetSync

---
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] smallestSubarrays(int[] nums) {
+        int n = nums.length;
+        int[] lastSeen = new int[30];
+        int[] res = new int[n];
+        for(int i=0;i<n;i++) res[i] = 1;
+
+        for(int i = n-1;i>=0;i--){
+            for(int bit =0;bit<30;bit++){
+                if((nums[i] & 1<< bit) > 0) lastSeen[bit] = i;
+                res[i] = Math.max(res[i],lastSeen[bit] -i+1);
+
+            }
+        }
+        return res;
+    }
+}
