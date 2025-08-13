686381db9b7de0263706571093d5f280f998fc67
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-08-13 12:56:07 +0530

    Time: 8 ms (84.41%) | Memory: 44.2 MB (39.26%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -1,11 +1,6 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-       
-        Set<Integer> hashSet = new HashSet<>();
-        for(int i=0;i<=19;i++){
-            hashSet.add((int)Math.pow(3,i));
-        }
-
-        return n > 0 && hashSet.contains(n);
+        int max = (int)Math.pow(3,19);
+        return n > 0 && max % n == 0;
     }
 }
