b9949f04b56b53779f946a28ae86a2eabb7af989
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-08-23 20:38:29 +0530

    Time: 32 ms (23.09%) | Memory: 45.3 MB (95.35%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -5,26 +5,30 @@ class Solution {
             hmap.put(c, hmap.getOrDefault(c, 0) + 1);
         }
 
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        maxHeap.addAll(hmap.values());
-        Queue<int[]> coolDown = new LinkedList<>();
-        int time =0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        pq.addAll(hmap.values());
+        int time = 0;
 
-        while(!maxHeap.isEmpty() || !coolDown.isEmpty()){
-            time++;
-            if(!coolDown.isEmpty() && coolDown.peek()[0] == time){
-                maxHeap.offer(coolDown.poll()[1]);
-            }
+        while(!pq.isEmpty()){
+            ArrayList<Integer> list = new ArrayList<>();
+            int task =0;
+            int cycle = n+1;
 
-            if(!maxHeap.isEmpty()){
-                int freq = maxHeap.poll();
-                int remainingFreq = freq -1;
-                if(remainingFreq > 0){
-                    int next = time + n+1;
-                    coolDown.add(new int[] {next,remainingFreq});
+            while(cycle-- >0 && !pq.isEmpty()){
+                int curr = pq.poll();
+                if(curr > 1){
+                    list.add(curr - 1);
                 }
+                task++;
             }
+
+            for(int i : list){
+                pq.add(i);
+            }
+
+            time += pq.isEmpty() ? task : n+1;
         }
+
         return time;
     }
 }
