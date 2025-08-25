c6122286d53c939dd8359ec623a058d23003d08a
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-08-26 02:13:03 +0530

    Time: 2 ms (83.68%) | Memory: 46.1 MB (13.45%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length -1;
+        int farthest = 0;
+
+        for(int i=0;i<nums.length;i++){
+            if(i > farthest) return false;
+            farthest = Math.max(farthest,i+nums[i]);
+            if(farthest >= goal) return true;
+        }
+        return false;
+    }
+}
