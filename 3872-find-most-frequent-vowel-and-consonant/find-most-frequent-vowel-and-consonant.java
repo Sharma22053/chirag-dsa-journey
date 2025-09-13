08a4941d463fe28ae0d27534ff9f3c5081db40ea
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-09-13 21:32:23 +0530

    Time: 3 ms (60.85%) | Memory: 42.9 MB (31.34%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
@@ -1,29 +1,15 @@
 class Solution {
     public int maxFreqSum(String s) {
-        Map<Character,Integer> hmapVowel = new HashMap<>();
-        Map<Character,Integer> hmapCon = new HashMap<>();
-        for(char ch : s.toCharArray()){
-            if(isVowel(ch)){
-                hmapVowel.put(ch,hmapVowel.getOrDefault(ch,0) + 1);
-            } else {
-                hmapCon.put(ch,hmapCon.getOrDefault(ch,0) + 1);
-            }
-        }
-
-    
-
+        Map<Character,Integer> hmap = new HashMap<>();
         int maxVowel = 0,maxCon =0;
-         int maxValue = Integer.MIN_VALUE;
-        for (Map.Entry<Character, Integer> entry : hmapVowel.entrySet()) {
-            if (entry.getValue() > maxVowel) {
-                maxVowel = entry.getValue();
-            }
-        }
 
-        for (Map.Entry<Character, Integer> entry : hmapCon.entrySet()) {
-            if (entry.getValue() > maxCon) {
-                maxCon = entry.getValue();
-                
+        for(char ch : s.toCharArray()){
+            hmap.put(ch,hmap.getOrDefault(ch,0) + 1);
+            int count = hmap.get(ch);
+            if(isVowel(ch)){
+                maxVowel = Math.max(maxVowel,count);
+            } else {
+                maxCon = Math.max(maxCon,count);
             }
         }
 
