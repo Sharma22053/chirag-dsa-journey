0095e84d877a417ba83c1ee4185c6088d3f09a97
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-09-20 03:39:28 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (33.21%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,30 +1,32 @@
 class Solution {
-    int[][] directions = new int[][]{{0,1},{0,-1},{-1,0},{1,0}};
+    int[][] directions = new int[][] { { 0, 1 }, { 0, -1 }, { -1, 0 }, { 1, 0 } };
     int fillColor;
     int initialColor;
+
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int[][] result = image;
         this.fillColor = color;
         this.initialColor = result[sr][sc];
         
-       if(fillColor == initialColor) return image;
 
-       dfs(result,sr,sc);
-       return result; 
-        
+        if (fillColor == initialColor)
+            return image;
+
+        dfs(result, sr, sc);
+        return result;
 
     }
 
-    private void dfs(int[][] result, int row,int col){
-        if(row < 0 || row >= result.length || col<0 || col>= result[0].length){
+    private void dfs(int[][] result, int row, int col) {
+        if (row < 0 || row >= result.length || col < 0 || col >= result[0].length || result[row][col] != initialColor) {
             return;
         }
 
-        if(result[row][col] != initialColor) return;
+       
 
         result[row][col] = fillColor;
-        for(int[] dir : directions){
-            dfs(result,row+dir[0],col+dir[1]);
+        for (int[] dir : directions) {
+            dfs(result, row + dir[0], col + dir[1]);
         }
     }
 }
