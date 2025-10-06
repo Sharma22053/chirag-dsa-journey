3bf7cd95e8783e861e6048248a506fff09109000
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-10-06 18:53:44 +0530

    Time: 606 ms (67.31%) | Memory: 55.1 MB (55.45%) - LeetSync

---
+++ 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int minimumDifference(int[] nums) {
+        int totalSum =0;
+        for(int i : nums) totalSum += i;
+        int target = totalSum / 2;
+        int half = nums.length / 2;
+        int best = Integer.MAX_VALUE;
+        int bestSum = Integer.MAX_VALUE;
+
+        Map<Integer,List<Integer>> map1 = new HashMap<>();
+        Map<Integer,List<Integer>> map2 = new HashMap<>();
+
+        for(int i=0;i<=half;i++){
+            map1.put(i,new ArrayList<>());
+            map2.put(i,new ArrayList<>());
+        }
+
+        helper(nums,0,half,0,0,map1);
+        helper(nums,half,nums.length,0,0,map2);
+
+        for(int i=0;i<=half;i++){
+            List<Integer> list1 = map1.get(i);
+            List<Integer> list2 = map2.get(half-i);
+
+            Collections.sort(list1);
+            Collections.sort(list2);
+
+            int l =0,r = list2.size()-1;
+            while(l<list1.size() && r >=0){
+                int sum = list1.get(l) + list2.get(r);
+                int diff = Math.abs(target-sum);
+                if(diff  < best){
+                    best = diff;
+                    bestSum = sum;
+                }
+                if(sum < target)l++;
+             else r--;
+            }
+
+        }
+        return Math.abs(totalSum - 2 * bestSum);
+    }
+
+    private void helper(int[] nums,int start,int end,int sum,int count,Map<Integer,List<Integer>> map){
+        if(start == end){
+            map.get(count).add(sum);
+            return;
+        }
+
+        helper(nums,start+1,end,sum+nums[start],count+1,map);
+        helper(nums,start+1,end,sum,count,map);
+    }
+}
