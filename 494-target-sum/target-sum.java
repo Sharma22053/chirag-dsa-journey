0515a63655cf2f4345e8b8cffce1d918faf23304
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-10-08 03:26:53 +0530

    Time: 4 ms (91.42%) | Memory: 43.4 MB (50.27%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,14 +1,29 @@
+
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-        return helper(nums,target,0,0);
-    }
-    private int helper(int[] nums,int target,int index,int currentSum){
-        if(index == nums.length){
-            if(currentSum == target) return 1;
-            else return 0;
+    Integer[][]dp;
+    public int findTargetSumWays(int[] arr, int d) {
+        // code here
+        
+        int totalSum = 0;
+        for(int i: arr){
+            totalSum += i;
         }
-        int plus = helper(nums,target,index+1,currentSum+nums[index]);
-        int minus = helper(nums,target,index+1,currentSum-nums[index]);
-        return plus + minus;
+        if((totalSum - d) < 0 || (totalSum - d) % 2 != 0 ) return 0;
+        int target = (totalSum - d) / 2;
+        dp = new Integer[arr.length+1][target+1];
+        return helper(arr,target,0,0);
+        
+    }
+    private int helper(int[] arr,int target,int sum,int index){
+        if(index == arr.length) return sum == target ? 1 : 0 ;
+       
+        if(dp[index][sum] != null) return dp[index][sum];
+        
+        int take = 0;
+        if(sum + arr[index] <= target){
+            take =  helper(arr,target,sum+arr[index],index+1);
+        }
+        int leave = helper(arr,target,sum,index+1);
+        return dp[index][sum] = take + leave;
     }
 }
