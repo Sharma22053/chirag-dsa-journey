58af776992c67a3d901b250b6ac53311e6219186
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-10-12 03:49:41 +0530

    Time: 19 ms (62.15%) | Memory: 51.1 MB (66.82%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -2,26 +2,23 @@ class Solution {
     public int numDistinct(String s, String t) {
         int sLength = s.length();
         int tLength = t.length();
-        int[][] dp = new int[sLength + 1][tLength + 1];
-        for (int i = 0; i <= sLength; i++) {
-            Arrays.fill(dp[i], -1);
+
+        int[][] dp = new int[sLength+1][tLength+1];
+
+        for(int i=0;i<=sLength;i++){
+            dp[i][0] = 1;
         }
-        return helper(s, t, sLength - 1, tLength - 1, dp);
-    }
-
-    private int helper(String s, String t, int i, int j, int[][] dp) {
-        if (j < 0)
-            return 1;
-        if (i < 0)
-            return 0;
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        if (s.charAt(i) == t.charAt(j)) {
-            return dp[i][j] = helper(s, t, i - 1, j - 1, dp) + helper(s, t, i - 1, j, dp);
+        for(int i=1;i<=sLength;i++){
+            for(int j=1;j<=tLength;j++){
+                if(i == 0) dp[i][j] = 0;
+                if(j == 0) dp[i][j] = 1;
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
         }
-
-        return dp[i][j] = helper(s, t, i - 1, j, dp);
+        return dp[sLength][tLength];
     }
 }
