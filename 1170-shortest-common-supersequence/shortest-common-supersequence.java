f5705d9686e68ffd4d4a618c910a71191b005df6
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-10-12 03:05:55 +0530

    Time: 23 ms (37.24%) | Memory: 51 MB (13.13%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.java
@@ -1,29 +1,33 @@
 class Solution {
     public String shortestCommonSupersequence(String str1, String str2) {
-        int n = str1.length();
-        int m = str2.length();
+        int str1Length = str1.length();
+        int str2Length = str2.length();
 
-        int[][] dp = new int[n+1][m+1];
+        int[][] dp = new int[str1Length+1][str2Length+1];
 
-       
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                if(i==0 || j==0) dp[i][j] = 0;
-                else if(str1.charAt(i-1) == str2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1] + 1;
-                } else {
-                    dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
-                }
+        for(int i=0;i<=str1Length;i++){
+        for(int j=0;j<=str2Length;j++){
+            if(i == 0 || j== 0) dp[i][j] = 0;
+
+            else if (str1.charAt(i-1) == str2.charAt(j-1)){
+                dp[i][j] = 1 + dp[i-1][j-1];
+            }
+            else {
+                dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
             }
         }
+
+        }
+
         StringBuilder sb = new StringBuilder();
-        int row = n , column = m;
+        int row = str1Length , column = str2Length;
         while(row > 0 && column >0){
             if(str1.charAt(row-1) == str2.charAt(column-1)){
                 sb.append(str1.charAt(row-1));
                 row--;
                 column--;
-            } else if(dp[row-1][column] > dp[row][column-1]){
+            }
+            else if(dp[row-1][column] > dp[row][column-1] ){
                 sb.append(str1.charAt(row-1));
                 row--;
             } else {
@@ -32,12 +36,12 @@ class Solution {
             }
         }
 
-        while(row>0){
+        while(row > 0){
             sb.append(str1.charAt(row-1));
             row--;
         }
 
-        while(column>0){
+        while(column > 0){
             sb.append(str2.charAt(column-1));
             column--;
         }
