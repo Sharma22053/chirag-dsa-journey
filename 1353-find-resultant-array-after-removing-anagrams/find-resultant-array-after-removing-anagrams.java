f6b5d544c8c00bd7dc0470848fd3113ad2d688b8
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-10-13 14:58:53 +0530

    Time: 2 ms (93.48%) | Memory: 44.9 MB (48.48%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
@@ -1,29 +1,21 @@
 class Solution {
-    
     public List<String> removeAnagrams(String[] words) {
         List<String> result = new ArrayList<>();
-        for(String s : words){
-            result.add(s);
-        }
-        int i=1;
-        while(i<result.size()){
-            String s1 = result.get(i);
-            String s2 = result.get(i-1);
-            if(isAnagram(s1,s2)){
-                result.remove(s1);
-                continue;
+        String previous = "";
+
+        for(String word : words){
+            String current = getCanonical(word);
+            if(!current.equals(previous)){
+                result.add(word);
+                previous = current;
             }
-            i++;
-           
         }
         return result;
     }
 
-    private boolean isAnagram(String s1, String s2){
-        char[] s1Array =s1.toCharArray();
-        char[] s2Array = s2.toCharArray();
-        Arrays.sort(s1Array);
-        Arrays.sort(s2Array);
-        return (new String(s1Array).equals(new String (s2Array))); 
+    private String getCanonical(String current){
+        char[] ch = current.toCharArray();
+        Arrays.sort(ch);
+        return new String(ch);
     }
 }
