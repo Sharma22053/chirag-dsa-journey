f908c229d54a9a7096d8d14fa277a78ef8693ac3
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-10-23 19:40:57 +0530

    Time: 7 ms (97.95%) | Memory: 56.7 MB (78.57%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,38 +1,36 @@
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> result = new ArrayList<>();
-        generate(result,new ArrayList<>(),0,s);
+        generate(result, new ArrayList<>(), 0, s);
         return result;
 
     }
 
-    static void generate( List<List<String>> result,ArrayList<String> temp,
-    int index,String s){
-        if(index == s.length()){
+    static void generate(List<List<String>> result, ArrayList<String> temp,
+            int index, String s) {
+        if (index == s.length()) {
             result.add(new ArrayList<>(temp));
             return;
         }
-        for(int i= index;i<s.length();i++){
-            String substring = s.substring(index,i+1);
-            if(isPalindrome(substring)){
-            temp.add(substring);
-            generate(result,temp,i+1,s);
-            temp.remove(temp.size() -1);
+        for (int i = index; i < s.length(); i++) {
+            String substring = s.substring(index, i + 1);
+            if (isPalindrome(substring)) {
+                temp.add(substring);
+                generate(result, temp, i + 1, s);
+                temp.remove(temp.size() - 1);
             }
 
         }
-        
 
     }
 
-    static boolean isPalindrome(String s){
+    static boolean isPalindrome(String s) {
         int n = s.length();
-       
-        int i=0;
-        int j = n-1;
-        while(i<j){
-           
-                if (s.charAt(i++) != s.charAt(j--)) {
+        int i = 0;
+        int j = n - 1;
+        while (i < j) {
+
+            if (s.charAt(i++) != s.charAt(j--)) {
                 return false;
             }
 
