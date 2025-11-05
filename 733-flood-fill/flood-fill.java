2102ab04001468b6e2b952ac4fca05682c208970
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-11-06 02:43:01 +0530

    Time: 1 ms (42.99%) | Memory: 46.8 MB (5.28%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,26 +1,31 @@
 class Solution {
-    int[][] directions = new int[][] { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
-    int initialColour;
-    int fillColour;
+    int[][] directions = { {0, 1}, {0, -1}, {1, 0}, {-1, 0} };
 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int[][] result = image;
-        this.initialColour = result[sr][sc];
-        this.fillColour = color;
+        int initialColour = image[sr][sc];
+        if (initialColour == color) return image;
 
-        if (fillColour == initialColour)
-            return image;
-        dfs(result,sr,sc);
-        return result;
+        int m = image.length;
+        int n = image[0].length;
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[] {sr, sc});
+        image[sr][sc] = color;
 
-    }
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int r = current[0];
+            int c = current[1];
 
-    private void dfs(int[][] result, int sr, int sc) {
-        if (sr < 0 || sc < 0 || sr >= result.length || sc >= result[0].length || result[sr][sc] != initialColour)
-            return;
-        result[sr][sc] = fillColour;
-        for (int[] dirs : directions) {
-            dfs(result, sr + dirs[0], sc + dirs[1]);
+            for (int[] dir : directions) {
+                int newR = r + dir[0];
+                int newC = c + dir[1];
+
+                if (newR >= 0 && newC >= 0 && newR < m && newC < n && image[newR][newC] == initialColour) {
+                    image[newR][newC] = color;
+                    queue.offer(new int[] {newR, newC});
+                }
+            }
         }
+        return image;
     }
 }
