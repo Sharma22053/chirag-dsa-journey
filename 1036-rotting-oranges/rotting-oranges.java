33c168807bf92fcada87ee7a1e86fbe184e16dba
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-11-06 01:51:27 +0530

    Time: 2 ms (66.76%) | Memory: 44 MB (7.03%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,44 +1,54 @@
 class Solution {
-    public int orangesRotting(int[][] grid) {
-        int countFresh = 0;
-        int m = grid.length;
-        int n = grid[0].length;
+    int[][] directions = { { 0, 1 }, { 1, 0 }, { -1, 0 }, { 0, -1 } };
 
+    public int orangesRotting(int[][] grid) {
+        Queue<int[]> queue = new ArrayDeque<>();
         int[][] visit = grid;
-        Queue<int[]> queue = new LinkedList<>();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(visit[i][j] == 2) queue.offer(new int[] {i,j});
-                if(visit[i][j] == 1) countFresh++;
+        int rowLength = grid.length;
+        int columnLength = grid[0].length;
+        int freshCount = 0;
+        for (int i = 0; i < rowLength; i++) {
+            for (int j = 0; j < columnLength; j++) {
+                if (grid[i][j] == 1) {
+                    freshCount++;
+                }
+                if (grid[i][j] == 2) {
+                    queue.offer(new int[] { i, j });
+                }
             }
         }
-
-        if(countFresh == 0) return 0;
-        if(queue.isEmpty()) return -1;
+        if (freshCount == 0)
+            return 0;
+        if (queue.size() == 0)
+            return -1;
         int minutes = -1;
-        int[][] directions = {{1,0} , {-1,0} , {0,-1}, {0,1}};
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int size = queue.size();
-            while( size-- > 0){
-                int[] cell = queue.poll();
-                int x = cell[0];
-                int y = cell[1];
-                for(int[] dir : directions){
-                    int i = x + dir[0];
-                    int j = y + dir[1];
-                    if(i >= 0 && i<m && j>=0 && j<n && visit[i][j] == 1){
-                        visit[i][j] = 2;
-                        countFresh--;
-                        queue.offer(new int[]{i,j});
+            minutes++;
+            for (int i = 0; i < size; i++) {
+                int[] current = queue.poll();
+                int row = current[0];
+                int column = current[1];
+
+                for (int[] dirs : directions) {
+                    int newRow = dirs[0] + row;
+                    int newColumn = dirs[1] + column;
+                    if (isValid(newRow, newColumn, rowLength, columnLength) && visit[newRow][newColumn] == 1) {
+                        freshCount--;
+                        queue.offer(new int[] { newRow, newColumn });
+                        visit[newRow][newColumn] = 2;
                     }
                 }
             }
-            minutes++;
+
         }
-        if(countFresh == 0) return minutes;
-        return -1;
-        
+        if (freshCount != 0)
+            return -1;
+        return minutes;
 
     }
-    
+
+    private boolean isValid(int row, int column, int rowLength, int columnLength) {
+        return row >= 0 && column >= 0 && row < rowLength && column < columnLength;
+    }
 }
