e4640f0cd5ee3826d6bfe67c2c40713b158c24e9
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-11-08 18:13:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (9.00%) - LeetSync

--- 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.java
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.java
@@ -1,20 +1,11 @@
 class Solution {
     public int minimumOneBitOperations(int n) {
-        if(n == 0) return 0;
-        long[] ops = new long[32];
-        ops[0] = 1;
-        for(int i=1;i<32;i++){
-            ops[i] = (2 * ops[i-1]) + 1;
-        }
-        int result = 0;
-        int sign = 1;
-        for(int i= 31;i>=0;i--){
-            if(( (1 << i) & n) == 0) continue;
-            if(sign > 0) result += ops[i];
-            else result -= ops[i];
-            sign = sign * -1;
-        }
-        return result;
-        
+        int ans = n;
+        ans ^= ans >> 16;
+        ans ^= ans >> 8;
+        ans ^= ans >> 4;
+        ans ^= ans >> 2;
+        ans ^= ans >> 1;
+        return ans;
     }
 }
