ea7999925d0389492486def6dc197008eb0a297e
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-11-11 20:09:11 +0530

    Time: 4 ms (18.71%) | Memory: 47.8 MB (10.16%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,51 +1,46 @@
 class Solution {
-    int[][] directions = { { 0, 1 }, { 1, 0 }, { -1, 0 }, { 0, -1 } };
-
     public void solve(char[][] board) {
-        int n = board.length;
-        int m = board[0].length;
-
-        int[][] visited = new int[n][m];
-
-        for (int i = 0; i < m; i++) {
-            if (board[0][i] == 'O')
-                dfs(board, 0, i, visited, m, n);
+        int[][] directions = {{0,1},{0,-1},{1,0},{-1,0}};
+        int rows = board.length;
+        int column = board[0].length;
+        boolean[][] visit = new boolean[rows][column];
+        
+        Queue<int[]> queue = new ArrayDeque<>();
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<column;j++){
+        if ((i == 0 || j == 0 || i == rows-1 || j == column-1) && board[i][j] == 'O'){
+            queue.offer(new int[]{i,j});
+            board[i][j] = '#';
         }
-
-        for (int i = 0; i < n; i++) {
-            if (board[i][m - 1] == 'O')
-                dfs(board, i, m - 1, visited, m, n);
-        }
-        for (int i = 0; i < m; i++) {
-            if (board[n - 1][i] == 'O')
-                dfs(board, n - 1, i, visited, m, n);
-        }
-        for (int i = 0; i < n; i++) {
-            if (board[i][0] == 'O')
-                dfs(board, i, 0, visited, m, n);
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == 'O' && visited[i][j] == 0)
-                    board[i][j] = 'X';
             }
         }
-    }
 
-    private void dfs(char[][] board, int row, int col, int[][] visited, int m, int n) {
-        visited[row][col] = 1;
-
-        for (int[] dir : directions) {
-            int newRow = dir[0] + row;
-            int newCol = dir[1] + col;
-
-            if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m
-                    && board[newRow][newCol] == 'O' && visited[newRow][newCol] == 0) {
-                dfs(board, newRow, newCol, visited, m, n);
-
+        while(!queue.isEmpty()){
+            int[] current = queue.poll();
+            int currentRow = current[0];
+            int currentColumn = current[1];
+            for(int[] dirs : directions){
+                int newRow = currentRow + dirs[0];
+                int newColumn = currentColumn + dirs[1];
+                if(isValid(newRow,newColumn,rows,column) && 
+                  board[newRow][newColumn] == 'O'){
+                   
+                     board[newRow][newColumn] = '#';
+                     queue.offer(new int[] {newRow,newColumn});
+                  }
             }
-
         }
+        
+for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < column; j++) {
+                if (board[i][j] == 'O') board[i][j] = 'X';
+                else if (board[i][j] == '#') board[i][j] = 'O';
+            }
+        }
+
     }
+    private boolean isValid(int r, int c, int rows,int column){
+        return r>=0 && c>=0 && r<rows && c < column;
+    }
+
 }
