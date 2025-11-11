5e2fadd80a98f03f55df4e89968cfa30cd14ad8f
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-11-11 19:31:38 +0530

    Time: 17 ms (96.22%) | Memory: 43.7 MB (49.23%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.java
+++ 474-ones-and-zeroes/ones-and-zeroes.java
@@ -1,25 +1,23 @@
 class Solution {
     public int findMaxForm(String[] strs, int m, int n) {
-        int[][][] dp = new int[strs.length][m+1][n+1];
-        return helper(strs,0,m,n,dp);
+        int[][] dp = new int[m + 1][n + 1];
+        for (String s : strs) {
+            int zeros = 0, ones = 0;
+            for (char ch : s.toCharArray()) {
+                if (ch == '0')
+                    zeros++;
+                else
+                    ones++;
+            }
+
+            for (int i = m; i >= zeros; i--) {
+                for (int j = n; j >= ones; j--) {
+                    int take = 1 + dp[i - zeros][j - ones];
+                    int leave = dp[i][j];
+                    dp[i][j] = Math.max(take, leave);
+                }
+            }
+        }
+        return dp[m][n];
     }
-    private int helper(String[] strs,int index,int m,int n,int[][][] dp){
-       if(index == strs.length) return 0;
-       if(dp[index][m][n] != 0) return dp[index][m][n];
-
-       int zeros = 0 , ones = 0;
-       for(char ch : strs[index].toCharArray()){
-        if(ch  == '0') zeros++;
-        else ones++;
-       }
-
-       int leave = helper(strs,index+1,m,n,dp);
-       int take = 0;
-       if(m >= zeros && n>= ones){
-        take = 1 + helper(strs,index+1,m-zeros,n-ones,dp);
-       }
-
-       return dp[index][m][n] = Math.max(take,leave);
-    }
-
 }
