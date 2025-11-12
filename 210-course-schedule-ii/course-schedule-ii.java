bae0a0cc51e5ca3475fd10a24feb894fdc16da78
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-11-13 03:00:22 +0530

    Time: 4 ms (93.73%) | Memory: 46.8 MB (13.57%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,47 +1,42 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj = buildGraph(numCourses,prerequisites);
-        int[] visit = new int[numCourses];
+        List<List<Integer>> adjacency = buildGraph(prerequisites,numCourses);
         Stack<Integer> stack = new Stack<>();
+        int[] visit = new int[numCourses];
         for(int i=0;i<numCourses;i++){
-            if(visit[i] == 0 && dfs(i,adj,stack,visit)){
+            if(visit[i] == 0 && dfs(i,adjacency,visit,stack)){
                 return new int[0];
             }
         }
-
         int[] result = new int[numCourses];
         for(int i=0;i<numCourses;i++){
             result[i] = stack.pop();
         }
         return result;
-        
     }
-
-
-    private boolean dfs(int node,List<List<Integer>> adj,Stack<Integer> stack,int[] visit){
+    private boolean dfs(int node,List<List<Integer>> adjacency,int[] visit,
+    Stack<Integer> stack){
         if(visit[node] == 1) return true;
         if(visit[node] == 2) return false;
         visit[node] = 1;
-        for(int i: adj.get(node)){
-            if(dfs(i,adj,stack,visit)){
-                return true;
-            }
+        for(int i : adjacency.get(node)){
+            if(dfs(i,adjacency,visit,stack)) return true;
         }
-        visit[node] = 2;
+        visit[node]=2;
         stack.push(node);
         return false;
+
     }
 
-    private List<List<Integer>> buildGraph(int numCourses,int[][] prerequisites){
-        List<List<Integer>> adj = new ArrayList<>();
+    private List<List<Integer>> buildGraph(int[][] prerequisites,int numCourses){
+        List<List<Integer>> result = new ArrayList<>();
         for(int i=0;i<numCourses;i++){
-            adj.add(new ArrayList<>());
-
+            result.add(new ArrayList<>());
         }
 
-        for(int[] edge : prerequisites){
-            adj.get(edge[1]).add(edge[0]);
+        for(int[] nodes : prerequisites){
+            result.get(nodes[1]).add(nodes[0]);
         }
-        return adj;
+        return result;
     }
 }
