8f7e7336f28b22d295cdfe77ed6eb17f4b29448d
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-11-13 02:42:00 +0530

    Time: 2 ms (34.53%) | Memory: 47.1 MB (8.53%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,22 +1,27 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        int n = graph.length;
-        int[] visit = new int[n];
-        for(int i=0;i<n;i++){
-            if(visit[i] == 0 && !dfs(i,graph,visit,1)){
-                return false;
+        int row = graph.length;
+        int column = graph[0].length;
+        int[] colors = new int[row];
+        Arrays.fill(colors,-1);
+        for(int i=0;i<row;i++){
+           if(colors[i] == -1){
+             Queue<Integer> queue = new LinkedList<>();
+        queue.offer(i);
+        colors[i] = 0;
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+            for(int neighbour : graph[node]){
+                if(colors[neighbour] == -1){
+                    colors[neighbour] = 1 - colors[node];
+                    queue.offer(neighbour);
+                }
+                else if(colors[neighbour] == colors[node]){
+                    return false;
+                }
             }
         }
-        return true;
-    }
-
-    private boolean dfs(int node,int[][] graph,int[] visit,int toFill){
-        
-        visit[node] = toFill;
-        for(int i : graph[node]){
-            if((visit[i] == 0 && !dfs(i,graph,visit,-toFill)) || visit[i] == toFill){
-                return false;
-            } 
+           }
         }
         return true;
     }
