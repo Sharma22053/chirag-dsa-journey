cdd0b65d96ee779010e85a17cceb37b98cb3e0b3
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-11-16 16:48:48 +0530

    Time: 5 ms (57.07%) | Memory: 46.3 MB (9.16%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
@@ -2,15 +2,15 @@ class Solution {
     public int numSub(String s) {
         int mod = 1000000007;
         long result = 0;
-        long continuous = 0;
+        long ones = 0;
         int n = s.length();
         for(int i=0;i<n;i++){
             char ch = s.charAt(i);
             if(ch == '1'){
-                continuous++;
-                result = (result + continuous) % mod;
+                ones++;
+                result = (result + ones) % mod;
             } else {
-                continuous = 0;
+                ones = 0;
             }
         }
        
