3220712db0da0abe1921998c89bfc77c54b49c41
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-12-03 03:09:29 +0530

    Time: 5 ms (82.11%) | Memory: 46.6 MB (41.50%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,44 +1,35 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adjacency = buildGraph(prerequisites,numCourses);
+        List<List<Integer>> adjacency = new ArrayList<>();
+        Queue<Integer> queue = new ArrayDeque<>();
+        int[] result = new int[numCourses];
+        int index = 0;
+        for (int i = 0; i < numCourses; i++) {
+            adjacency.add(new ArrayList<>());
+        }
         int[] inDegree = new int[numCourses];
-        for(int[] edge : prerequisites){
+        for (int[] edge : prerequisites) {
+            adjacency.get(edge[1]).add(edge[0]);
             inDegree[edge[0]]++;
         }
-        Queue<Integer> queue = new LinkedList<>();
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for(int i=0;i<numCourses;i++){
-            if(inDegree[i] == 0){
+
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
                 queue.offer(i);
             }
         }
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int node = queue.poll();
-            list.add(node);
-            for(int neighbour : adjacency.get(node)){
+            result[index++] = node;
+            for (int neighbour : adjacency.get(node)) {
                 inDegree[neighbour]--;
-                if(inDegree[neighbour] == 0){
+                if (inDegree[neighbour] == 0) {
                     queue.offer(neighbour);
                 }
             }
         }
-        if(list.size() != numCourses) return new int[0];
-        int[] result = new int[numCourses];
-        for(int i=0;i<numCourses;i++){
-            result[i] = list.get(i);
-        }
-        return result;
-    }
-   
-    private List<List<Integer>> buildGraph(int[][] prerequisites,int numCourses){
-        List<List<Integer>> result = new ArrayList<>();
-        for(int i=0;i<numCourses;i++){
-            result.add(new ArrayList<>());
-        }
 
-        for(int[] nodes : prerequisites){
-            result.get(nodes[1]).add(nodes[0]);
-        }
-        return result;
+        return index == numCourses ? result : new int[0];
     }
+
 }
