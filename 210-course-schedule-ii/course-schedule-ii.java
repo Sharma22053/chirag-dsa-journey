cf6f80c0978d4f28ec3353156211e746ece16a06
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-12-03 03:04:20 +0530

    Time: 6 ms (50.21%) | Memory: 46.6 MB (41.50%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,33 +1,35 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adjacency = buildGraph(prerequisites,numCourses);
-        Stack<Integer> stack = new Stack<>();
-        int[] visit = new int[numCourses];
+        int[] inDegree = new int[numCourses];
+        for(int[] edge : prerequisites){
+            inDegree[edge[0]]++;
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        ArrayList<Integer> list = new ArrayList<Integer>();
         for(int i=0;i<numCourses;i++){
-            if(visit[i] == 0 && dfs(i,adjacency,visit,stack)){
-                return new int[0];
+            if(inDegree[i] == 0){
+                queue.offer(i);
             }
         }
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+            list.add(node);
+            for(int neighbour : adjacency.get(node)){
+                inDegree[neighbour]--;
+                if(inDegree[neighbour] == 0){
+                    queue.offer(neighbour);
+                }
+            }
+        }
+        if(list.size() != numCourses) return new int[0];
         int[] result = new int[numCourses];
         for(int i=0;i<numCourses;i++){
-            result[i] = stack.pop();
+            result[i] = list.get(i);
         }
         return result;
     }
-    private boolean dfs(int node,List<List<Integer>> adjacency,int[] visit,
-    Stack<Integer> stack){
-        if(visit[node] == 1) return true;
-        if(visit[node] == 2) return false;
-        visit[node] = 1;
-        for(int i : adjacency.get(node)){
-            if(dfs(i,adjacency,visit,stack)) return true;
-        }
-        visit[node]=2;
-        stack.push(node);
-        return false;
-
-    }
-
+   
     private List<List<Integer>> buildGraph(int[][] prerequisites,int numCourses){
         List<List<Integer>> result = new ArrayList<>();
         for(int i=0;i<numCourses;i++){
