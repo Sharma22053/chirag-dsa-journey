ceb70e68865a368002a916c730d147b8b7b06688
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-12-08 14:13:35 +0530

    Time: 13 ms (56.16%) | Memory: 42.1 MB (49.69%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.java
+++ 2037-count-square-sum-triples/count-square-sum-triples.java
@@ -3,8 +3,9 @@ class Solution {
         int result = 0;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                int k = (int) Math.sqrt(i * i + j * j + 1.0);
-                if (k <= n && k * k == i * i + j * j) {
+                int s = i * i + j * j;
+                int k = (int) Math.sqrt(s);
+                if (k <= n && k * k == s) {
                     result++;
                 }
             }
