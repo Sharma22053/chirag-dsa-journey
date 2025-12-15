b9cb0458bb313246b91ccfc7110690f48ad29aee
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-12-16 00:37:03 +0530

    Time: 1 ms (100.00%) | Memory: 52.6 MB (66.03%) - LeetSync

--- 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
+++ 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
@@ -9,17 +9,13 @@
  */
 
 class Solution {
-    TreeNode toFind;
     public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
-        toFind = target;
-        return helper(cloned);
-    }
-    private TreeNode helper(TreeNode root){
-        if(root == null) return null;
-        if(root.val == toFind.val) return root;
-        TreeNode left = helper(root.left);
-        TreeNode right = helper(root.right);
+        if(original == null) return null;
+        if(original == target) return cloned;
 
-        return left == null ? right :left;
+        TreeNode left = getTargetCopy(original.left,cloned.left,target);
+        if(left != null) return left;
+
+        return getTargetCopy(original.right,cloned.right,target);
     }
 }
