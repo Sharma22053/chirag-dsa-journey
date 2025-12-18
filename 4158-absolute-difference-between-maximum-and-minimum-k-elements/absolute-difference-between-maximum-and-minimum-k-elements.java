ef6b591ca660b2d3486523acfa354b4bb472c78b
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-12-19 04:16:10 +0530

    Time: 5 ms (98.51%) | Memory: 47 MB (25.48%) - LeetSync

--- 4158-absolute-difference-between-maximum-and-minimum-k-elements/absolute-difference-between-maximum-and-minimum-k-elements.java
+++ 4158-absolute-difference-between-maximum-and-minimum-k-elements/absolute-difference-between-maximum-and-minimum-k-elements.java
@@ -1,24 +1,13 @@
 class Solution {
     public int absDifference(int[] nums, int k) {
         Arrays.sort(nums);
-        Queue<Integer> minHeap = new PriorityQueue<>();
-        Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        int minSum = 0, maxSum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            minHeap.offer(nums[i]);
-            maxHeap.offer(nums[i]);
-            if (maxHeap.size() > k) {
-                maxHeap.poll();
-            }
-            if (minHeap.size() > k) {
-                minHeap.poll();
-            }
+        int n = nums.length;
+        int minSum = 0;
+        int maxSum = 0;
+        for (int i = 0; i < k; i++) {
+            minSum += nums[i];
+            maxSum += nums[n - 1 - i];
         }
-        while (!maxHeap.isEmpty() && !minHeap.isEmpty()) {
-            minSum += maxHeap.poll();
-            maxSum += minHeap.poll();
-        }
-        return Math.abs(maxSum - minSum);
-
+        return Math.abs(minSum-maxSum);
     }
 }
