43bc3881772e74461af6f09fea5f64cca7afdf60
Sharma22053 <118891279+Sharma22053@users.noreply.github.com>
2025-12-29 15:41:44 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (25.39%) - LeetSync

---
+++ 1755-defuse-the-bomb/defuse-the-bomb.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int[] result = new int[n];
+        if (k == 0)
+            return result;
+        int start = 1, end = k, sum = 0;
+        if (k < 0) {
+            start = n - Math.abs(k);
+            end = n - 1;
+        }
+        for (int i = start; i <= end; i++)
+            sum += code[i];
+        for (int i = 0; i < n; i++) {
+            result[i] = sum;
+            sum -= code[(start) % n];
+            sum += code[(end + 1) % n];
+            start++;
+            end++;
+        }
+        return result;
+    }
+}
